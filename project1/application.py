97423a710706f40bf550f4702492182624820789
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2020-04-21 21:13:06 +0530

    changes done

--- project1/application.py
+++ project1/application.py
@@ -25,30 +25,36 @@ SESSION = db()
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    if session.get("email") is not None:
+        return render_template("login.html")
+    else:
+        return redirect("/register")
 
 @app.route("/register",methods = ["GET", "POST"])
 def signup():
-    if request.method == "POST":
-        name = request.form.get("name")
-        email = request.form.get("email")
-        password = request.form.get("password")
-        print("name : ", name)
-        print("email : ", email)
-        timestamp = datetime.datetime.now()
-        # print("timestamp ::", timestamp)
-        user = User(name=name,email=email,password=password,timestamp=timestamp)
-        # print("user added")
-        try:
-            SESSION.add(user)
-            # print("add done")
-            SESSION.commit()
-            # print("commit done")
-            return render_template("hello.html", name = name)
-        except:
-            return render_template("error.html")
-    else:
-        return render_template("registration.html")
+    if session.get("email") is not None:
+        return render_template("login.html")
+    else:    
+        if request.method == "POST":
+            name = request.form.get("name")
+            email = request.form.get("email")
+            password = request.form.get("password")
+            print("name : ", name)
+            print("email : ", email)
+            timestamp = datetime.datetime.now()
+            # print("timestamp ::", timestamp)
+            user = User(name=name,email=email,password=password,timestamp=timestamp)
+            # print("user added")
+            try:
+                SESSION.add(user)
+                # print("add done")
+                SESSION.commit()
+                # print("commit done")
+                return render_template("registration.html", name = "Hi, your registration is successful")
+            except:
+                return render_template("registration.html", name = "User is already exist")
+        else:
+            return render_template("registration.html")
 
 @app.route("/admin",methods = ["GET"])
 def tabledetails():
@@ -62,9 +68,9 @@ def authorized():
         email = request.form.get("email")
         password = request.form.get("password")
         thisuser = db.query(User).get(email)
-        session["email"] = email
         if thisuser != None:
             if password == thisuser.password:
+                session["email"] = email
                 return render_template("login.html", name = name)
             else:
                 return render_template("registration.html", name = "Please enter correct password")
